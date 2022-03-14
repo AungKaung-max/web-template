7d518bd148f60676d545e0b82ddd94130a341867
AungKaung-max <willing672k@gmail.com>
2022-03-14 22:11:23 +0630

    web project

---
+++ Website Templete/js/main.js
@@ -0,0 +1,41 @@
+jQuery(window).on('load', function() {
+    jQuery("#preloader").delay(2000).fadeOut("normal");
+});
+
+$(document).ready(function(){
+	$(".toggle").click(function(){
+		$(".nav").slideToggle('show-menu');
+	});
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+// When the user scrolls down ()px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("mybtn").style.display = "block";
+    } else {
+        document.getElementById("mybtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
